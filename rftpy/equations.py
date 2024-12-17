e1a475ac98f45fee09d055a4e7b720c4274c336c
noahthebugg <noah.bugajczyk@gmail.com>
2024-12-17 21:31:28 +0100

    feature(functions): added functions

--- rftpy/equations.py
+++ rftpy/equations.py
@@ -2,6 +2,7 @@
 
 import math
 
+
 def linear_eccentricity(a: float, b: float) -> float:
     """
     Calculate the linear eccentricity coefficient.
@@ -10,6 +11,7 @@ def linear_eccentricity(a: float, b: float) -> float:
     :return: linear eccentricity [km]
     """
     return math.sqrt(a**2 - b**2)
+
 
 def numerical_eccentricity(a: float, b: float) -> float:
     """
@@ -21,6 +23,7 @@ def numerical_eccentricity(a: float, b: float) -> float:
     c: float = linear_eccentricity(a, b)
     return c / a
 
+
 def semi_latus_rectum(a: float, b: float) -> float:
     """
     Calculate the Semi-Latus Rectum.
@@ -29,6 +32,7 @@ def semi_latus_rectum(a: float, b: float) -> float:
     :return: Semi-Latus Rectum [km]
     """
     return b**2 / a
+
 
 def r_nu(p: float, e: float, v: float) -> float:
     """
@@ -40,6 +44,7 @@ def r_nu(p: float, e: float, v: float) -> float:
     """
     return p / (1 + e * math.cos(v))
 
+
 def r_perigee(a: float, e: float) -> float:
     """
     Calculate the radius at perigee.
@@ -48,6 +53,7 @@ def r_perigee(a: float, e: float) -> float:
     :return: distance at perigee [km]
     """
     return a * (1 - e)
+
 
 def r_apogee(a: float, e: float) -> float:
     """
@@ -58,6 +64,7 @@ def r_apogee(a: float, e: float) -> float:
     """
     return a * (1 + e)
 
+
 def orbit_interval(a: float, mu: float) -> float:
     """
     Calculate the interval of an orbiting body.
@@ -67,14 +74,27 @@ def orbit_interval(a: float, mu: float) -> float:
     """
     return 2 * math.pi * math.sqrt(a**3 / mu)
 
+
 def interval2a(interval: float, mu: float) -> float:
     """
-    Calculate the semi-major axis by the interval of an orbiting body.
+    Calculate the semi-major axis by the orbit interval of an orbiting body.
     :param interval: interval of an orbiting body [s]
     :param mu: standard gravitational parameter [km^3/s^2]
     :return: semi-major axis [km]
     """
     return ((interval / (2 * math.pi)) ** 2 * mu) ** (1/3)
+
+
+def velocity2a(v: float, mu: float, r: float) -> float:
+    """
+    Calculate the semi-major axis by the velocity of an orbiting body.
+    :param v: velocity of an orbiting body [km/s]
+    :param mu: standard gravitational parameter [km^3/s^2]
+    :param r: distance of an orbiting body [km]
+    :return: semi-major axis [km]
+    """
+    return -1 / (v**2 / mu - (2/r))
+
 
 def vis_viva(mu: float, r: float, a: float) -> float:
     """
@@ -86,6 +106,7 @@ def vis_viva(mu: float, r: float, a: float) -> float:
     """
     return math.sqrt(mu * (2/r - 1/a))
 
+
 def red_vis_viva(mu: float, r: float) -> float:
     """
     Reduced Vis-Viva-Equation: Calculate the speed of an orbiting body.
@@ -94,6 +115,7 @@ def red_vis_viva(mu: float, r: float) -> float:
     :return: speed of an orbiting body [km/s]
     """
     return math.sqrt(mu / r)
+
 
 def v_cosmic1(mu: float, r: float) -> float:
     """
@@ -104,6 +126,7 @@ def v_cosmic1(mu: float, r: float) -> float:
     """
     return red_vis_viva(mu, r)
 
+
 def v_cosmic2(mu: float, r: float) -> float:
     """
     Calculate the 2nd cosmic velocity.
@@ -112,6 +135,7 @@ def v_cosmic2(mu: float, r: float) -> float:
     :return: v_esc (2nd cosmic velocity) [km/s]
     """
     return math.sqrt(2 * (mu / r))
+
 
 def angular_momentum(r: float, v: float) -> float:
     """
@@ -122,6 +146,7 @@ def angular_momentum(r: float, v: float) -> float:
     """
     return r * v
 
+
 def spc_angular_momentum(h: float, m: float) -> float:
     """
     Calculate the specific angular momentum of a satellite.
@@ -130,6 +155,7 @@ def spc_angular_momentum(h: float, m: float) -> float:
     :return: specific angular momentum [km^2/kgs]
     """
     return h / m
+
 
 def drive_req(v1: float, v2: float, di:float) -> float:
     """
@@ -141,6 +167,7 @@ def drive_req(v1: float, v2: float, di:float) -> float:
     """
     return math.sqrt(v1**2 + v2**2 - (2 * v1 * v2 * math.cos(math.radians(di))))
 
+
 def n(mu: float, a: float) -> float:
     """
     Calculate the average motion.
@@ -149,6 +176,7 @@ def n(mu: float, a: float) -> float:
     :return: n (average motion) [1/s]
     """
     return math.sqrt(mu / a**3)
+
 
 def domega(n: float, j: float, r_e: float, a: float, i: float, e: float) -> float:
     """
@@ -163,6 +191,7 @@ def domega(n: float, j: float, r_e: float, a: float, i: float, e: float) -> floa
     """
     return - (3/2) * n * j * (r_e / a)**2 * (math.cos(math.radians(i)) / (1 - e**2)**2)
 
+
 def ziolkowski(c: float, m1: float, m2: float) -> float:
     """
     Ziolkowski rocket equation.
@@ -173,6 +202,7 @@ def ziolkowski(c: float, m1: float, m2: float) -> float:
     """
     return c * math.log((m1 / m2), math.e)
 
+
 def mass_flow(m: float, dt: float) -> float:
     """
     Calculate the mass flow.
@@ -181,6 +211,7 @@ def mass_flow(m: float, dt: float) -> float:
     :return: mass flow [kg/s]
     """
     return m / dt
+
 
 def c_e(F: float, dotm: float) -> float:
     """
@@ -191,6 +222,7 @@ def c_e(F: float, dotm: float) -> float:
     """
     return F / dotm
 
+
 def boost(c_e: float, dotm: float) -> float:
     """
     Calculate the rocket boost.
@@ -200,43 +232,47 @@ def boost(c_e: float, dotm: float) -> float:
     """
     return c_e * dotm
 
+
 def grav_force(M: float, m: float, r: float) -> float:
     """
     Newtons law: Calculate the gravitational force.
-    :param M: Planet mass [kg]
-    :param m: Satellite mass [kg]
-    :param r: Radius [km]
-    :return: Gravitational force [N]
+    :param M: planet mass [kg]
+    :param m: satellite mass [kg]
+    :param r: radius [km]
+    :return: gravitational force [N]
     """
     G: float = 6.67430e-11  # Gravitational constant [m^3/kgs^-2]
     return -G * (M * m) / r ** 2
 
+
 def di(v1: float, dv: float,) -> float:
     """
     Calculate the change of inclination.
-    :param v1: Velocity of the circular orbit [km/s]
-    :param dv: Change of velocity [km/s]
+    :param v1: velocity of the circular orbit [km/s]
+    :param dv: change of velocity [km/s]
     :return: change of inclination [deg]
     """
     return math.asin(dv / (2 * v1)) * 2
 
+
 def v_0_lat(r: float, lat: float) -> float:
     """
     Calculate the starting velocity depending on the latitude.
-    :param r: Planet radius [km]
-    :param lat: Latitude [deg]
-    :return: Starting velocity [km/s]
+    :param r: planet radius [km]
+    :param lat: latitude [deg]
+    :return: starting velocity [km/s]
     """
     return ((2 * math.pi * r) / (24 * 3600)) * math.cos(math.radians(lat))
+
 
 def alpha(r2_p: float, v2_p: float, r1: float, v2: float) -> float:
     """
     Calculate the angle between an circular and elliptical orbit.
-    :param r2_p: Radius at perigee [km]
-    :param v2_p: Velocity at perigee [km/s]
-    :param r1: Radius of circular orbit [km]
-    :param v2: Velocity on circular orbit [km/s]
-    :return: Anngle [deg]
+    :param r2_p: radius at perigee [km]
+    :param v2_p: velocity at perigee [km/s]
+    :param r1: radius of circular orbit [km]
+    :param v2: velocity on circular orbit [km/s]
+    :return: angle [deg]
     """
     return math.degrees(math.acos(
         (r2_p * v2_p) / (r1 * v2)
